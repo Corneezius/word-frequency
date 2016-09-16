70c706eb1eab07ca0b44241ae3a1e686c4daf310
Nicolas <nickh93@outlook.com>
2016-09-16 10:48:09 -0400

    add logic for fifth test

--- tests/WordFrequencyTest.php
+++ tests/WordFrequencyTest.php
@@ -48,12 +48,26 @@
             //ASSERT
             $this->assertEquals($expected_outcome, $test_result);
         }
-        function test_FullWordInOneSentence()
+        function test_FullWordOnceInSentence()
         {
           //Arrange
           $sentence = "a friend of mine just had a birthday";
           $word = "birthday";
           $expected_outcome = 1;
+          $word_frequency_instance = new WordFreqiency($sentence, $word);
+
+          //ACT
+          $test_result = $word_frequency_instance->checkWords($sentence, $word);
+
+          //ASSERT
+          $this->assertEquals($expected_outcome, $test_result);
+        }
+        function test_FullWordMultipleTimesInSentence()
+        {
+          //Arrange
+          $sentence = "I will have one salad and one soup";
+          $word = "one";
+          $expected_outcome = 2;
           $word_frequency_instance = new WordFreqiency($sentence, $word);
 
           //ACT
