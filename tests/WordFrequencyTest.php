edcfe3e78dbead3e9c314bf50ead5559ac296e2a
Nicolas <nickh93@outlook.com>
2016-09-16 10:42:56 -0400

    add logic for third test

--- tests/WordFrequencyTest.php
+++ tests/WordFrequencyTest.php
@@ -19,12 +19,27 @@
             $this->assertEquals($expected_outcome, $test_result);
         }
 
-        function test_1leLetterWordMultipleleLetterSentence()
+        function test_1leLetterWordOnceMultipleleWordSentence()
         {
             //Arrange
             $sentence = "an apple is a fruit";
             $word = "a";
             $expected_outcome = 1;
+            $word_frequency_instance = new WordFreqiency($sentence, $word);
+
+            //ACT
+            $test_result = $word_frequency_instance->checkWords($sentence, $word);
+
+            //ASSERT
+            $this->assertEquals($expected_outcome, $test_result);
+        }
+
+        function test_1leLetterWordMultipleTimesWInMultipleleWordSentence()
+        {
+            //Arrange
+            $sentence = "a friend of mine just had a birthday";
+            $word = "a";
+            $expected_outcome = 2;
             $word_frequency_instance = new WordFreqiency($sentence, $word);
 
             //ACT
